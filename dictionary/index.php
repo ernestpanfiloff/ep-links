cf004b9adc142cdc01322044c49d7937ff3fc583
ernest <ernestas.du@gmail.com>
2022-11-24 20:57:04 +0000

    a

--- dictionary/index.php
+++ dictionary/index.php
@@ -48,7 +48,7 @@ include 'connect.php';
             $definition = $card['definition'];
             $date = $card['date'];
 
-            echo ' <div class="col-md-4">
+            echo ' <div class="col-md-4 mb-5">
             <div class="card">
                 <div class="card-body">
                   <h2 class="card-title">'.$title.'</h2>
