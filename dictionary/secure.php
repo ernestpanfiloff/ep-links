d77e6afa5259a3967e3299e5cc2743f746475eea
ernest <ernestas.du@gmail.com>
2022-11-24 21:55:02 +0000

    a

--- dictionary/secure.php
+++ dictionary/secure.php
@@ -11,7 +11,6 @@ else
     {?>
 
             <form method="POST" action="secure.php">
-            User <input type="text" name="user"></input><br/>
             Pass <input type="password" name="pass"></input><br/>
             <input type="submit" name="submit" value="Go"></input>
             </form>
