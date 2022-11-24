4139ee29596dd61ba937a21eeb2a16304e2fa5c6
ernest <ernestas.du@gmail.com>
2022-11-24 17:16:43 +0000

    init dictionary

---
+++ dictionary/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$con = new mysqli("159.65.26.109", "tpckdrwwjd", "W8RPDVrnrm", "tpckdrwwjd");
+
+if ($con) {
+    echo "Connected";
+} else {
+    die("Connection failed: " . mysqli_error($con));
+}
+
+?>
