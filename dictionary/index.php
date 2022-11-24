ef11271b1641954083f56e1e961617f5d8beffad
ernest <ernestas.du@gmail.com>
2022-11-24 21:01:55 +0000

    changes

--- dictionary/index.php
+++ dictionary/index.php
@@ -14,7 +14,7 @@ include 'connect.php';
 
     <nav class="navbar navbar-expand-lg bg-success">
         <div class="container-sm">
-          <a class="navbar-brand text-light" href="#">🐼 Panda Dictionary</a>
+          <a class="navbar-brand text-light" href="index.php">🐼 Panda Dictionary</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
