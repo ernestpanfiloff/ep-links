f792c8b3ca4dad7a581078c21856361ce5129fcf
ernest <ernestas.du@gmail.com>
2022-11-24 23:00:32 +0000

    a

--- dictionary/definition.php
+++ dictionary/definition.php
@@ -117,15 +117,6 @@ if(isset($_POST['submit'])) {
           
     </div>    
 
-    <SCRIPT language="JavaScript">
-var password; 
-var pass1="1228"; 
-password=prompt('Enter Password',' '); 
-if (password==pass1) 
-alert('Correct Password! Click OK to Enter!'); 
-else { window.location="http://ernestandpanda.com/dictionary/definition.php"; 
-} 
-</SCRIPT>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
